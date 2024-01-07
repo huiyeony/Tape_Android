595e25cb6a35485f50e1d68000323f73e4260cdc
JavaNo0b <98101954+JavaNo0b@users.noreply.github.com>
2024-01-07 13:33:23 +0900

    AlbumFragment_IncludedSongs_viewPager 구현

--- app/src/main/java/com/janob/tape_aos/MainActivity.kt
+++ app/src/main/java/com/janob/tape_aos/MainActivity.kt
@@ -54,10 +54,11 @@ class MainActivity : AppCompatActivity() {
                 }
 
                 R.id.profile_nav -> {
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.main_fm, ProfileFragment())
-                        .commitAllowingStateLoss()
-                    return@setOnItemSelectedListener true
+//                    supportFragmentManager.beginTransaction()
+//                        .replace(R.id.main_fm, ProfileFragment())
+//                        .commitAllowingStateLoss()
+//                    return@setOnItemSelectedListener true
+                    startActivity(Intent(this, AlbumActivity::class.java))
                 }
             }
             false
